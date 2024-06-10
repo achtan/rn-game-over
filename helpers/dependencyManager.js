f7400a36107a383fe17a60dc7faba3302147c7f0
David Durika <durika.d@gmail.com>
2024-06-10 13:44:19 +0200

    Add support for pnpm

--- helpers/dependencyManager.js
+++ helpers/dependencyManager.js
@@ -1,15 +1,21 @@
 const shell = require("shelljs");
 
 const PACKAGE_LOCK_FILE = "package-lock.json";
+const PNPM_LOCK_FILE = "pnpm-lock.json";
 const NPM_COMMAND = "npm";
 const YARN_COMMAND = "yarn";
+const PNPM_COMMAND = "pnpm";
 
 const getDependencyManager = function() {
   if (shell.test("-f", PACKAGE_LOCK_FILE)) {
     return NPM_COMMAND;
-  } else {
-    return YARN_COMMAND;
   }
+
+  if (shell.test("-f", PNPM_LOCK_FILE)) {
+    return PNPM_COMMAND;
+  }
+
+  return YARN_COMMAND;
 };
 
 module.exports = getDependencyManager();
